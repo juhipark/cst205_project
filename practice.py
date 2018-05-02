3d977c7f747d5b0a3e1f9612a3859b6dadbf6547
Cesar Aldrete <cesaraaldrete@gmail.com>
2018-05-02 15:01:19 -0700

    pushing updated files

--- practice.py
+++ practice.py
@@ -1,25 +1,32 @@
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
-from PIL import Image
 import urllib
 import os
 from googletrans import Translator
 
-site = 'https://en.wikipedia.org/wiki/Chair'
-req = Request(site, headers={'User-Agent' : 'Mozilla/5.0'})
+def imageSearch(result):
+	site = 'https://en.wikipedia.org/wiki/' + result
+	req = Request(site, headers={'User-Agent' : 'Mozilla/5.0'})
 
-resp = urlopen(req)
-bs_obj = BeautifulSoup(resp.read(), 'html.parser')
+	resp = urlopen(req)
+	bs_obj = BeautifulSoup(resp.read(), 'html.parser')
 
-count = 0
+	count = 0
+	pics_lst = []
 
-for tag in bs_obj.findAll("img"):
-	count+=1
-	if(count < 4):
-		temp = tag.get('alt')
-		target = tag.get('src')
-		print(target)
+	for tag in bs_obj.findAll("img"):
+		count+=1
+		if(count < 4):
+			target = tag.get('src')
+			pics_lst.append(target)
 
-"""
-Save image list as pics_lst
-"""
+	for i in pics_lst:
+		print(i)
+
+def main():
+	trans = Translator()
+
+	r = trans.translate('chaise')
+
+	print(r)
+main()
